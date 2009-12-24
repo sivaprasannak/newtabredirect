ac873bb8a71e277a1d9f8298a3933435e38f3c74
james.schubert <james.schubert@ae232420-f03c-11de-a89a-43a960051dd3>
2009-12-24 03:44:42 +0000

    Version 0.0.1.105

---
+++ options.js
@@ -0,0 +1,71 @@
+var aboutPages = ["about:blank","about:version", "about:plugins","about:cache", "about:memory","about:histograms","about:dns"];
+var popularPages = { 
+	"Facebook":"www.facebook.com",
+	"MySpace":"www.myspace.com",
+	"Twitter":"www.twitter.com",
+	"Digg":"www.digg.com",
+	"Delicious":"www.delicious.com",
+	"Slashdot":"www.slashdot.org"
+};
+	
+// save options to localStorage.
+function save_options() {
+	var url = $('#custom-url').val();
+	if(url == ""){
+		url = aboutPages[0];
+	}
+	
+	if( $.inArray(String(url), aboutPages) || isValidURL(url)) {
+		save(true,url);
+	} else {
+		save(false,url);
+	}		
+}
+
+function save(good,url) {
+	if(good) {
+		chrome.extension.getBackgroundPage().setUrl(url);
+		$('#status').text("Options Saved.");
+	} else {
+		$('#status').text( url + " is invalid. Try again (http:// is required)");	
+	}
+	
+	$('#status').css("display", "block");
+	setTimeout(function(){
+		$('#status').slideUp("fast").css("display", "none");
+	}, 1050);
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+	var url = chrome.extension.getBackgroundPage().url || "http://www.facebook.com/";	
+	 $('#custom-url').val(url);
+}
+
+function isValidURL(url) {
+	var urlRegxp = /^(http:\/\/www.|https:\/\/www.|ftp:\/\/www.|www.){1}([\w]+)(.[\w]+){1,2}$/;
+	if (urlRegxp.test(url) != true) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function saveQuickLink(url){
+	var uurl = unescape(url);
+	 $('#custom-url').val(uurl);
+	 save(true,uurl);
+	 return false;
+}
+
+$(document).ready(function(){
+	restore_options();
+	$.each(aboutPages, function() {
+		var anchor = "<a href=\"javascript:saveQuickLink('"+this+"');\">"+this+"</a>";
+		$('#abouts').append("<li>" + anchor + "</li>");
+	});
+	$.each(popularPages, function(k,v) {
+		var anchor = "<a href=\"javascript:saveQuickLink('"+v+"');\">"+k+"</a>";
+		$('#popular').append("<li>" + anchor + "</li>");
+	});
+});
